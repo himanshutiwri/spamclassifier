631abf1d7ffebf8afff8d658b41bd07303f445e7
HimanshuTiwri <himanshutiwari1061@gmail.com>
2025-09-27 00:05:54 +0530

    Download NLTK punkt and stopwords for deployment

--- app.py
+++ app.py
@@ -4,37 +4,31 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import pickle
 import string
+import os
 
 app = Flask(__name__)
 
-nltk.download('punkt')
-nltk.download('stopwords')
+# Ensure NLTK data is downloaded
+nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
+if not os.path.exists(nltk_data_dir):
+    os.mkdir(nltk_data_dir)
+
+nltk.data.path.append(nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
 
 ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))  # load once
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+    y = [i for i in text if i.isalnum()]
+    text = [i for i in y if i not in stop_words and i not in string.punctuation]
+    text = [ps.stem(i) for i in text]
 
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
+    return " ".join(text)
 
 def predict_spam(message):
     # Preprocess
@@ -51,13 +45,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        input_sms = request.form['message']
-        result = predict_spam(input_sms)
-        return render_template('index.html', result=result)  # Pass 'result' to the template
-
+    input_sms = request.form['message']
+    result = predict_spam(input_sms)
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
     model = pickle.load(open('model.pkl', 'rb'))
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
